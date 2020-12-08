0fe5125e23df5810669c2466838f153acef1d858
Sudeep <sudeep.jain@ehi.com>
2020-12-08 14:16:51 -0600

    Day 8 Part 1...

---
+++ day8/handheld.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"../lib"
+  "strconv"
+)
+
+func main() {
+	lines := lib.ReadInput()
+  fmt.Println(lastAccumulatorValPart1(lines))
+
+}
+
+func lastAccumulatorValPart1(lines []string) int {
+  accumulator := 0
+	m := map[int]int{}
+	for i:=0;i<len(lines);i++ {
+    v := strings.Fields(lines[i])
+
+    switch v[0] {
+    case "acc":
+      //increase or decrease accumulator
+      arg, err := strconv.Atoi(v[1])
+      lib.Check(err)
+      accumulator = accumulator + arg
+    case "nop":
+      //do nothing
+    case "jmp":
+      //go to new instruction
+      arg, err := strconv.Atoi(v[1])
+      lib.Check(err)
+      i = i + arg -1
+    }
+    if(i<0){
+      break;
+    }
+		if _, exists := m[i]; exists {
+			return accumulator
+		} else {
+			m[i] = accumulator
+		}
+    //fmt.Println(v, i, accumulator)
+	}
+  return accumulator
+}
